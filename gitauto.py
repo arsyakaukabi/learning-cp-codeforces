58d92c287e81ea2e235e2b27deab4bf6f2573405
Mohamad Arsya Kaukabi <arsyakaukabi@gmail.com>
2023-07-17 21:41:51 +0700

    Added new solutions

--- gitauto.py
+++ gitauto.py
@@ -9,8 +9,8 @@ try:
         ["git", "commit", "-m", commit_message], check=True
     )  # Commit changes with the provided message
     subprocess.run(
-        ["git", "push", "origin", "main"], check=True
-    )  # Push changes to the origin main branch
+        ["git", "push", "origin", "master"], check=True
+    )  # Push changes to the origin master branch
     print("Commit and push successful!")
 except subprocess.CalledProcessError as e:
     print("Commit and push failed. Error:", e)
